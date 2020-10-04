18095ba49c666e8fd77a3c7bf3bd056512d02e57
Mithunavarshini <61896139+Mithunavarshini@users.noreply.github.com>
2020-10-04 01:24:33 -0700

    Add files via upload

---
+++ arithmetic operations.py
@@ -0,0 +1,21 @@
+#getting user inputs
+
+num1 = input ("Enter the num :")
+num2 = input ("Enter the num :")
+
+#performing arithmetic operations
+
+sum = float(num1) + float(num2)
+
+min = float(num1) - float(num2)
+
+mul = float(num1) * float(num2)
+
+div = float(num1) / float(num2)
+
+#printing the results
+
+print("The addition of two num is ",sum)
+print("The subtraction of two num is ",min)
+print("The multiplication of two num is ",mul)
+print("The division of two num is ",div )
