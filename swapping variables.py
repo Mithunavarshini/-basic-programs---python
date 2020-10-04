bdc161d431e5e230bafed6a0f52f497794af8351
Mithunavarshini <61896139+Mithunavarshini@users.noreply.github.com>
2020-10-04 01:25:22 -0700

    Add files via upload

---
+++ swapping variables.py
@@ -0,0 +1,16 @@
+#getting user input
+
+x = input ("Enter value of x :")
+y = input ("Enter value of y :") 
+
+#creating temporry variable and swapping the values
+
+temp = x
+x = y
+y = temp
+
+#printing the swapped values
+
+
+print("The value of x is",x)
+print("The value of y is",y)
