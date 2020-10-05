4a3c862f311c87a03b60277727de45930e5c358a
Mithunavarshini <61896139+Mithunavarshini@users.noreply.github.com>
2020-10-04 23:13:15 -0700

    Add files via upload

---
+++ odd or even.py
@@ -0,0 +1,13 @@
+#getting user input
+
+num = int(input("Enter a number : "))
+
+#checking the condition
+
+if(num % 2) == 0:
+    
+    print("{0} is even number ".format(num))
+    
+else:
+    
+    print("{0} is odd number ".format(num))
